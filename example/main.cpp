e0f7b1ffea62330851728bdc48e1fd685accb42e
nereus <fangyuping0521@gmail.com>
2023-06-05 00:02:17 +0800

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -5,16 +5,15 @@
 
 int main()
 {
-	std::shared_ptr<HttpClient> client = HttpClientBuilder::newBuilder().redirect(Redirect::NORMAL).userAgent("lwhttp/0.0.1").build();
-	URL url("https://www.google.com");
+	std::shared_ptr <HttpClient> client = HttpClientBuilder::newBuilder().redirect(Redirect::NORMAL).userAgent("lwhttp/0.0.1").build();
+	URL url("https://www.lwhttpd.org/demo/");
 	HttpRequest request = HttpRequestBuilder::newBuilder().url(url).GET().build();
 	HttpResponse response{};
 	if (0 < client->send(request, response))
 	{
-		std::cout << "status: " << static_cast<int>(response.getStatusCode()) << "/" << response.getReason() << std::endl;
+		std::cout << "Response:\nstatus: " << response.getStatusCode() << "/" << response.getReason() << std::endl;
 		auto data = response.getResponseBody()->getContent();
-		auto len = response.getResponseBody()->getBodyLength();
-		std::cout << "response length " << len << ", data:\n" << data << std::endl;
+		std::cout << "body:\n" << data << std::endl;
 	}
 	else
 	{
